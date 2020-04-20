6c12c09a9bba765b53304a2e87e60b74552857c2
MillicentHale <machale@perse.co.uk>
2020-04-20 13:55:02 +0100

    Stage 2

--- Game.py
+++ Game.py
@@ -1,18 +1,74 @@
+from itertools import product
+
+class GameError(Exception):
+    pass
 
 class Game:
 
+    dimension = 3
+    empty = " "
+    P1 = "o"
+    P2 = "x"
+
     def __init__(self):
-        pass
+        self.board = [[Game.empty for _ in range (Game.dimension)] for _ in range (Game.dimension)]
+        self._player = Game.P1
 
     def __repr__(self):
-        pass
+        def get(y, x):
+            if self.board[y][x] == "x":
+                return "x"
+            if self.board[y][x] == "o":
+                return "o"
+            else:
+                return " "
+        return f"""
+
+  1 2 3
+1 {get(0,0)}|{get(0,1)}|{get(0,2)} 
+  -----
+2 {get(1,0)}|{get(1,1)}|{get(1,2)}
+  -----
+3 {get(2,0)}|{get(2,1)}|{get(2,2)}
+
+
+It is {self._player}'s turn
+
+"""
 
     def play(self,row,col):
-        pass
+        row -= 1
+        col -= 1
+        self.board[row][col] = self._player
+        self._player = Game.P1 if self._player == Game.P2 else Game.P2
     
     @property
     def winner(self):
-        pass
+        for p in [Game.P1,Game.P2]:
+            for row in range(Game.dimension):
+                if all(self.board[row][col] is p for col in range(Game.dimension)):
+                    return p
+            for col in range(Game.dimension):
+                if all(self.board[row][col] is p for row in range(Game.dimension)):
+                    return p
+            # Diagonals
+            if all(self.board[i][i] is p for i in range(Game.dimension)):
+                return p
+            if all(self.board[i][2 - i] is p for i in range(Game.dimension)):
+                return p
+        # No winner
+        return None
+
+    @property
+    def drawn(self):
+        if all(self.board[r][c] is not Game.empty for (r,c) in product(range(Game.dimension),range(Game.dimension))) and self.winner == None:
+            return True
+        else:
+            return False
+
+
 
 if __name__ == "__main__":
+    g = Game()
+    print (g)
     pass
--- Ui.py
+++ Ui.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from game import Game, GameError
 
 class Ui(ABC):
 
@@ -15,7 +16,35 @@ class Gui(Ui):
 
 class Terminal(Ui):
     def __init__(self):
-        pass
+        self.__game = Game()
 
     def run(self):
-        pass
+        while not self.__game.winner and not self.__game.drawn:
+            print (self.__game)
+            while True:
+                try:
+                    row, col = int(input("Which row")), int(input("Which col"))
+                    if not 0 < row <= Game.dimension:
+                        raise GameError()
+                    if not 0 < col <= Game.dimension:
+                        raise GameError()
+                    if self.__game.board[row-1][col-1] != Game.empty:
+                        print("The space must be empty")
+                    else:
+                        break
+                except ValueError:
+                    print ("Row and column need to be in numbers")
+                except GameError:
+                    print ("Row and column need to be between 1 and 3 inclusive")
+            self.__game.play(row, col)
+                
+        if self.__game.drawn:
+            print (self.__game)
+            print("The game was drawn")
+        else:
+            print (self.__game)
+            print (f"The Winner was {self.__game.winner}")
+
+if __name__ == "__main__":
+    ui = Terminal()
+    ui.run()
